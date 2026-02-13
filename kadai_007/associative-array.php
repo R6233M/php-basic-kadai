a899daec1b6245d92c7bbcf2f4308b52bf2b84a2
R6233M <kukuri.hi6233@gmail.com>
2026-02-13 22:33:33 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $kadai_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        print_r($kadai_data);
+        ?>
+    </p>
+</body>
+
+</html>
