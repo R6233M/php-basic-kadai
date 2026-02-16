dc7b3e5bc31b54aeec141ae64f869fdb2621929f
R6233M <kukuri.hi6233@gmail.com>
2026-02-16 21:19:53 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // Foodクラスを定義する
+       class Food {
+        // プロパティを定義する
+           public $name;
+           public $price;
+
+        // priceプロパティの値を出力するメソッド
+            public function show_price() {
+            echo $this->price;
+            }
+
+        // コンストラクタ
+        public function __construct($name, $price) {
+            $this->name = $name;
+            $this->price = $price;
+        }
+       }
+
+        // Animalクラスを定義する
+        class Animal {
+            // プロパティを定義する
+            public $name;
+            public $height;
+            public $weight;
+
+            // heightプロパティの値を出力するメソッド
+            public function show_height() {
+            echo $this->height;
+            }
+
+            // コンストラクタ
+            public function __construct($name, $height, $weight) {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+            }
+        }
+        // Foodインスタンス化
+        $food = new Food('ラーメン', 1200);
+
+        // Animalインスタンス化
+        $animal = new Animal('dog', 5, 50);
+
+        // インスタンス$foodと$animalの各プロパティの値を出力する
+        print_r($food);
+        print_r($animal);
+       ?>
+   </p>
+</body>
+
+</html>
